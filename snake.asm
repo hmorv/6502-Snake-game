b0acc89dea42b084e5d05e17deac6a4d30f02dab
Hugo Moragues <hugo.moragues@gmail.com>
2018-01-28 09:12:46 +0100

    full subroutines remaining

--- snake.asm
+++ snake.asm
@@ -70,3 +70,198 @@ generateApplePosition:
   rts
 
 
+loop:
+  jsr readKeys
+  jsr checkCollision
+  jsr updateSnake
+  jsr drawApple
+  jsr drawSnake
+  jsr spinWheels
+  jmp loop
+
+
+readKeys:
+  lda sysLastKey
+  cmp #ASCII_w
+  beq upKey
+  cmp #ASCII_d
+  beq rightKey
+  cmp #ASCII_s
+  beq downKey
+  cmp #ASCII_a
+  beq leftKey
+  rts
+upKey:
+  lda #movingDown
+  bit snakeDirection
+  bne illegalMove
+
+  lda #movingUp
+  sta snakeDirection
+  rts
+rightKey:
+  lda #movingLeft
+  bit snakeDirection
+  bne illegalMove
+
+  lda #movingRight
+  sta snakeDirection
+  rts
+downKey:
+  lda #movingUp
+  bit snakeDirection
+  bne illegalMove
+
+  lda #movingDown
+  sta snakeDirection
+  rts
+leftKey:
+  lda #movingRight
+  bit snakeDirection
+  bne illegalMove
+
+  lda #movingLeft
+  sta snakeDirection
+  rts
+illegalMove:
+  rts
+
+
+checkCollision:
+  jsr checkAppleCollision
+  jsr checkSnakeCollision
+  rts
+
+
+checkAppleCollision:
+  lda appleL
+  cmp snakeHeadL
+  bne doneCheckingAppleCollision
+  lda appleH
+  cmp snakeHeadH
+  bne doneCheckingAppleCollision
+
+  ;eat apple
+  inc snakeLength
+  inc snakeLength ;increase length
+  jsr generateApplePosition
+doneCheckingAppleCollision:
+  rts
+
+
+checkSnakeCollision:
+  ldx #2 ;start with second segment
+snakeCollisionLoop:
+  lda snakeHeadL,x
+  cmp snakeHeadL
+  bne continueCollisionLoop
+
+maybeCollided:
+  lda snakeHeadH,x
+  cmp snakeHeadH
+  beq didCollide
+
+continueCollisionLoop:
+  inx
+  inx
+  cpx snakeLength          ;got to last section with no collision
+  beq didntCollide
+  jmp snakeCollisionLoop
+
+didCollide:
+  jmp gameOver
+didntCollide:
+  rts
+
+
+updateSnake:
+  ldx snakeLength
+  dex
+  txa
+updateloop:
+  lda snakeHeadL,x
+  sta snakeBodyStart,x
+  dex
+  bpl updateloop
+
+  lda snakeDirection
+  lsr
+  bcs up
+  lsr
+  bcs right
+  lsr
+  bcs down
+  lsr
+  bcs left
+up:
+  lda snakeHeadL
+  sec
+  sbc #$20
+  sta snakeHeadL
+  bcc upup
+  rts
+upup:
+  dec snakeHeadH
+  lda #$1
+  cmp snakeHeadH
+  beq collision
+  rts
+right:
+  inc snakeHeadL
+  lda #$1f
+  bit snakeHeadL
+  beq collision
+  rts
+down:
+  lda snakeHeadL
+  clc
+  adc #$20
+  sta snakeHeadL
+  bcs downdown
+  rts
+downdown:
+  inc snakeHeadH
+  lda #$6
+  cmp snakeHeadH
+  beq collision
+  rts
+left:
+  dec snakeHeadL
+  lda snakeHeadL
+  and #$1f
+  cmp #$1f
+  beq collision
+  rts
+collision:
+  jmp gameOver
+
+
+drawApple:
+  ldy #0
+  lda sysRandom
+  sta (appleL),y
+  rts
+
+
+drawSnake:
+  ldx snakeLength
+  lda #0
+  sta (snakeHeadL,x) ; erase end of tail
+
+  ldx #0
+  lda #1
+  sta (snakeHeadL,x) ; paint head
+  rts
+
+
+spinWheels:
+  ldx #0
+spinloop:
+  nop
+  nop
+  dex
+  bne spinloop
+  rts
+
+
+gameOver:
