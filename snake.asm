4903fb11ccd8b223ad8d34a9b2bcc876677bb255
Hugo Moragues <hugo.moragues@gmail.com>
2018-01-28 09:10:51 +0100

    snake + apple code

--- snake.asm
+++ snake.asm
@@ -26,3 +26,47 @@ define sysLastKey   $ff
   jsr init
   jsr loop
 
+init:
+  jsr initSnake
+  jsr generateApplePosition
+  rts
+
+
+initSnake:
+  lda #movingRight  ;start direction
+  sta snakeDirection
+
+  lda #4  ;start length (2 segments)
+  sta snakeLength
+  
+  lda #$11
+  sta snakeHeadL
+  
+  lda #$10
+  sta snakeBodyStart
+  
+  lda #$0f
+  sta $14 ; body segment 1
+  
+  lda #$04
+  sta snakeHeadH
+  sta $13 ; body segment 1
+  sta $15 ; body segment 2
+  rts
+
+
+generateApplePosition:
+  ;load a new random byte into $00
+  lda sysRandom
+  sta appleL
+
+  ;load a new random number from 2 to 5 into $01
+  lda sysRandom
+  and #$03 ;mask out lowest 2 bits
+  clc
+  adc #2
+  sta appleH
+
+  rts
+
+
